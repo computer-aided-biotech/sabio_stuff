28fb61d959f5d5b776604406a9015dce03492f71
carrascomj <carrascomurielj@gmail.com>
2022-01-21 18:39:40 +0100

    enhance: add pH and T info

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,95 @@
-use rust_sbml::{Model, UnitSIdRef};
+use std::collections::HashMap;
+use std::io::Read;
+use std::path::PathBuf;
+
+use rust_sbml::{LocalParameter, Model, UnitSIdRef};
+
+#[derive(Debug, argh::FromArgs)]
+/// Sabio SBML to TSV parameters.
+pub struct Args {
+    /// input SBML file from SABIO-rk.
+    #[argh(positional)]
+    file: PathBuf,
+}
+
+// Populate a map to go from SABIO ids in the model to the corresponding names
+fn get_species_map(model: &mut Model) -> HashMap<String, String> {
+    model
+        .species
+        .iter_mut()
+        .map(|(id, spec)| (id.to_owned(), spec.name.take().unwrap_or_default()))
+        .collect()
+}
+
+struct ExperimentalParameter {
+    param: LocalParameter,
+    ph: Option<String>,
+    t: Option<String>,
+}
+
+fn map_local_to_exp(
+    local_param: Vec<LocalParameter>,
+    sabiork: &Option<rust_sbml::rdf::Sabiork>,
+) -> Vec<ExperimentalParameter> {
+    let ph = sabiork
+        .as_ref()
+        .map(|sabiork| sabiork.get_ph().map(|ph| format!("{}", ph)).unwrap_or(String::from("NaN")));
+    let t = sabiork
+        .as_ref()
+        .map(|sabiork| sabiork.get_temperature().map(|t| format!("{}", t)).unwrap_or(String::from("NaN")));
+    local_param
+        .into_iter()
+        .map(|local_param| ExperimentalParameter {
+            param: local_param,
+            ph: ph.clone(),
+            t: t.clone(),
+        })
+        .collect()
+}
 
 fn main() {
-    let file_str = include_str!("../sabio597.xml");
-    let mut model = Model::parse(file_str).unwrap();
-    eprintln!("Parsed model {}", model.name.unwrap_or_default());
-    eprintln!("Model units: {:?}", model.model_units);
-    println!("id,value,unit");
+    let args: Args = argh::from_env();
+
+    let mut file_h = std::fs::File::open(args.file).expect("File must exist!");
+    let mut file_str = String::new();
+    file_h
+        .read_to_string(&mut file_str)
+        .expect("Unable to read the file");
+    let mut model = Model::parse(&file_str).unwrap();
+    let species = get_species_map(&mut model);
+
+    println!("id\tvalue\tunit\tspecies_name\tpH\tT");
     model
         .reactions
         .iter_mut()
         .filter_map(|(_id, r)| r.kinetic_law.take())
-        .flat_map(|kl| kl.list_of_local_parameters.local_parameter)
+        .flat_map(|kl| {
+            let sabiork = kl.annotation.unwrap().sabiork.take();
+            map_local_to_exp(
+                kl.list_of_local_parameters.local_parameter,
+                &sabiork,
+            )
+        })
         .for_each(|param| {
             println!(
-                "{},{},{:?}",
-                param.id,
-                param.value.unwrap_or_default(),
+                "{}\t{}\t{:?}\t{}\t{}\t{}",
+                param.param.id,
+                param.param.value.unwrap_or_default(),
                 param
+                    .param
                     .units
                     .as_ref()
-                    .unwrap_or(&UnitSIdRef::CustomUnit(String::from("")))
+                    .unwrap_or(&UnitSIdRef::CustomUnit(String::from(""))),
+                species
+                    .iter()
+                    .find_map(|(id, name)| if param.param.id.contains(id) {
+                        Some(name.as_str())
+                    } else {
+                        None
+                    })
+                    .unwrap_or(""),
+                param.ph.unwrap_or(String::from("NaN")),
+                param.t.unwrap_or(String::from("NaN")),
             )
         });
 }
